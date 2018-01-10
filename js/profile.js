a65dfea8b0098d060cf0ce8c50c1c56b5d4747c0
YoannaHS <yoannahernandez.s@gmail.com>
2018-01-10 14:44:10 -0500

    Añadiendo imagen al perfil

--- js/profile.js
+++ js/profile.js
@@ -1,9 +1,7 @@
 $(document).ready(function() {
  var myphoto = localStorage.getItem('imgprofile');
- var $ancor = $('#root');
  var $img = $('<img>');
- 
- $ancor.append($img);
-//  $img.attr('src', myphoto);
-$img.attr('src', myphoto);
+
+ $('#root').append($img);
+ $img.attr('src', myphoto);
 });
